03cc8c291876d179ffd76efc01db20b6b35ef33d
Matthijs Smit <matthijs@codesmit.nl>
2024-12-05 20:03:38 +0100

    Create starting point for case

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,36 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        fontFamily: {
+            "sans": "var(--font-bernina-sans)",
+            "heading": "var(--font-clan-pro)",
+        },
+        container:{
+            "center": true,
+        },
+        extend: {
+            colors: {
+                brand: {
+                    yellow: "rgb(255, 205, 0)",
+                    blue: "rgb(0, 61, 134)",
+                },
+                foreground: {
+                    DEFAULT: "rgb(0, 3, 7)",
+                    muted: "rgb(96, 96, 96)"
+                },
+                background: {
+                    DEFAULT: "rgb(255, 255, 255)",
+                    muted: "rgb(242, 242, 242)",
+                    border: "rgb(229, 229, 229)"
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
